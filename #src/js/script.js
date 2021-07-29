baa23eaa41595d284890640e52a75405ff36e52c
madac4 <anrei.bors@gmail.com>
2021-07-29 12:54:58 +0300

    mobile header

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,18 +1,41 @@
-// var i = 0;
-// function move() {
-//   if (i == 0) {
-//     i = 1;
-//     var elem = document.getElementById("myBar");
-//     var width = 1;
-//     var id = setInterval(frame, 3000);
-//     function frame() {
-//       if (width >= 100) {
-//         clearInterval(id);
-//         i = 0;
-//       } else {
-//         width++;
-//         elem.style.width = width + "%";
-//       }
-//     }
-//   }
-// }
+if (document.querySelector('.header-category__select')) {
+    const categorySelect = document.querySelector('.header-category__select');
+    const categoryDropdown = document.querySelector('.header-category__dropdown');
+    const categoryItems = categoryDropdown.querySelectorAll('.category-dropdown__item');
+    const overlay = document.querySelector('.header-category__overlay');
+
+    categorySelect.addEventListener('click', () => {
+        categorySelect.classList.toggle('open');
+        categoryDropdown.classList.toggle('open');
+        overlay.classList.toggle('open');
+        document.body.classList.toggle('lock');
+        document.addEventListener('click', (el) => {
+            if (el.target === overlay) {
+                categorySelect.classList.remove('open');
+                categoryDropdown.classList.remove('open');
+                overlay.classList.remove('open');
+                document.body.classList.remove('lock');
+            }
+        })
+    });
+
+    for (let i = 0; i < categoryItems.length; i++) {
+        categoryItems[i].addEventListener('click', function() {
+            const activeElement = document.getElementsByClassName('active');
+
+            if (activeElement.length > 0) {
+                activeElement[0].className = activeElement[0].className.replace(" active", "");
+            }
+
+            this.className += " active";
+        })
+    }
+}
+
+if (document.querySelector('.mobile-header')) {
+    const burger = document.querySelector('.burger');
+    burger.addEventListener('click', () => {
+        burger.classList.toggle('active');
+        document.body.classList.toggle('lock');
+    })
+}
